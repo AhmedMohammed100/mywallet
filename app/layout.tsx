6c3c0447618db835eae61272252ea839a55083ce
AhmedMohammed100 <ahmedtal650@gmail.com>
2024-10-08 11:13:20 +0000

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>TON Connect Demo</title>
+        <title>Connect TON Wallet</title>
+        <p>Farm Big<span></span>Earn $BIG</p>
       </head>
       <body>
         <TonConnectUIProvider manifestUrl="https://apricot-voluntary-emu-723.mypinata.cloud/ipfs/QmdNxmnbXhApfZsCFEKVLnB3g8Gpmq2nBn4jX6w9wNDD4V">
